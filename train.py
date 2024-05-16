b19ff800615d35e10f33b8f838772501226880e5
janahmedprg <jan.ahmed.prg@gmail.com>
2024-05-16 14:43:01 -0400

    init

---
+++ train.py
@@ -0,0 +1,80 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import sklearn
+import joblib
+import argparse
+import joblib
+import os
+import pandas as pd
+
+def model_fn(model_dir):
+    clf = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return clf
+
+if __name__ =='__main__':
+    print("[INFO] Extracting arguments")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--n_estimators", type=int, default=100)
+    parser.add_argument("--random_state", type=int, default=0)
+
+    parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+    parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
+    parser.add_argument("--train-file", type=str, default="fraudTrain.csv")
+    parser.add_argument("--test-file", type=str, default="fraudTest.csv")
+
+    args, _ = parser.parse_known_args()
+
+    print("SKLearn Version:", sklearn.__version__)
+    print("Joblib Version:", joblib.__version__)
+
+    print("[INFO] Reading data")
+    print()
+
+    df_train = pd.read_csv(os.path.join(args.train, args.train_file))
+    df_test = pd.read_csv(os.path.join(args.test, args.test_file))
+
+    df_train.drop(df_train.columns[0],axis=1, inplace=True)
+    df_test.drop(df_test.columns[0],axis=1,inplace=True)
+
+    df_train.drop(columns=["trans_date_trans_time", "cc_num", "merchant", "category", "first", "last", "gender", "street", "city", "state", "zip", "job", "dob", "trans_num"], inplace=True)
+    df_test.drop(columns=["trans_date_trans_time", "cc_num", "merchant", "category", "first", "last", "gender", "street", "city", "state", "zip", "job", "dob", "trans_num"], inplace=True)
+
+    
+    x_train=df_train.drop("is_fraud",axis=1)
+    y_train=df_train["is_fraud"]
+    x_test=df_test.drop("is_fraud",axis=1)
+    y_test=df_test["is_fraud"]
+
+    
+    print("Building training and testing datasets")
+    print() 
+
+
+    print("train data shape",x_train.shape)
+    print("test data shape",x_test.shape)
+    print("y train shape", y_train.shape)
+    print("y test shape", y_test.shape)
+
+    print("Training RandomForest Model....")
+    print()
+    model = RandomForestClassifier(n_estimators=args.n_estimators, random_state=args.random_state)
+    model.fit(x_train,y_train)
+    print()
+    
+    model_path = os.path.join(args.model_dir, "model.joblib")
+    joblib.dump(model, model_path)
+    print("Model persisted at", model_path)
+    print()
+    y_pred_test = model.predict(x_test)
+    test_acc = accuracy_score(y_test, y_pred_test)
+    test_rep = classification_report(y_test, y_pred_test)
+
+    print()
+    print("---- METRICS RESULTS FOR TESTING DATA ----")
+    print()
+    print("Total Rows are:", x_test.shape[0])
+    print("[TESTING] Model Accuracy is:", test_acc)
+    print("[TESTING] Testing Report:")
+    print(test_rep)
